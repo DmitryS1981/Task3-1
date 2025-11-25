80e75264ffdbdc129905cab558f34ce280f3e5af
DmitryS1981 <dir2@odis.su>
2025-11-25 23:00:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Барсик");
+        Cat cat2 = new Cat("Мурзик");
+        Dog dog1 = new Dog("Бобик");
+        Dog dog2 = new Dog("Шарик");
+        Dog dog3 = new Dog("Рекс");
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        System.out.println("Тестирование бега:");
+        cat1.run(150);
+        cat1.run(250);
+        dog1.run(400);
+        dog1.run(600);
+
+        System.out.println("\nТестирование плавания:");
+        cat2.swim(5);
+        dog2.swim(8);
+        dog2.swim(15);
+
+        System.out.println("Всего котов: " + Cat.getTotalCats());
+        System.out.println("Всего собак: " + Dog.getTotalDogs());
+        System.out.println("Всего животных: " + Animal.getTotalAnimals());
+    }
 }
