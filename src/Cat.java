417dec3bd81ba0e09c769c6d096583ae6ad68aa7
DmitryS1981 <dir2@odis.su>
2025-11-25 22:59:45 +0300

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class Cat {
+public class Cat extends Animal {
+    static int totalCats = 0;
+    public Cat(String name) {
+        super(name, 200, 0, false);
+        totalCats++;
+    }
+    static int getTotalCats() {
+        return totalCats;
+    }
 }
