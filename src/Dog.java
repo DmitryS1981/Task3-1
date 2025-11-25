dbdd5b9d4203fc1e0fb9e8d7541b4ec0602dce67
DmitryS1981 <dir2@odis.su>
2025-11-25 23:00:19 +0300

    Update Dog.java

--- src/Dog.java
+++ src/Dog.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
-public class Dog {
+class Dog extends Animal {
+    static int totalDogs = 0;
+    public Dog(String name) {
+        super(name, 500, 10, true);
+        totalDogs++;
+    }
+    static int getTotalDogs() {
+        return totalDogs;
+    }
 }
